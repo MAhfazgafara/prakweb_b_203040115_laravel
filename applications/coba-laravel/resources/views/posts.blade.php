f5933ccdf6e6ce58fce264d7a13aeb4e8ae456b0
MAhfazgafara <m.ahfaz.203040115@mail.unpas.ac.id>
2022-10-30 22:35:54 +0700

    menambahkan link author

--- applications/coba-laravel/resources/views/posts.blade.php
+++ applications/coba-laravel/resources/views/posts.blade.php
@@ -7,11 +7,23 @@
 <article class="mb-5">
 
     @foreach ($posts as $post)
-        <h2>
-            <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
-        </h2>
-        <P>{{ $post->excerpt }} </p>
+        <article class="mb-5 border-bottom pb-4">
+
+            <h2>
+                <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
+            </h2>
+
+            <p>By. Ahfaz Gafara in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
+            <P>{{ $post->excerpt }} </p>
+
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more..</a>
+
+        </article>
+
     @endforeach
+
 <a class="px-4 text-center btn btn-info btn-dark" href="/">Back to Home</a>
+
 @endsection
 
