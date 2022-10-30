d7c261876adcc70b1a2b40a341f97f5136df6cc9
MAhfazgafara <m.ahfaz.203040115@mail.unpas.ac.id>
2022-10-30 23:11:22 +0700

    Membuat Seeder

--- applications/coba-laravel/database/seeders/DatabaseSeeder.php
+++ applications/coba-laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +18,58 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Ahfaz Gafara',
+            'email' => 'ahfazgafara86@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+        
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit omnis explicabo cupiditate necessitatibus aspernatur, quae odit commodi sint, eos voluptatibus et reiciendis alias laboriosam repudiandae.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
