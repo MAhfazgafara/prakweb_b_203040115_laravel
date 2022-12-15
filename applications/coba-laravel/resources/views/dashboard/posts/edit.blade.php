0467d8acb4aa7c9d9a3918c83529d1a7982f8882
MAhfazgafara <m.ahfaz.203040115@mail.unpas.ac.id>
2022-12-15 11:38:17 +0700

    Preview, Update & Delete Image

--- applications/coba-laravel/resources/views/dashboard/posts/edit.blade.php
+++ applications/coba-laravel/resources/views/dashboard/posts/edit.blade.php
@@ -6,9 +6,10 @@
 </div>
 
 <div class="col-lg-8">   
-    <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+    <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5" enctype="multipart/form-data">
         @method('put')
         @csrf
+
         <div class="mb-3">
           <label for="title" class="form-label">Title</label>
           <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required autofocus value="{{ old('title', $post->title) }}">
@@ -18,6 +19,7 @@
             </div>
           @enderror
         </div>
+
         <div class="mb-3">
           <label for="slug" class="form-label">Slug</label>
           <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required value="{{ old('slug', $post->slug) }}">
@@ -26,6 +28,7 @@
               {{ $message }} 
           @enderror
         </div>
+
         <div class="mb-3">
           <label for="category" class="form-label">Category</label>
           <select class="form-select" name="category_id">
@@ -38,6 +41,23 @@
             @endforeach
           </select>
         </div>
+
+        <div class="mb-3">
+          <label for="image" class="form-label">Post Image</label>
+          <input type="hidden" name="oldImage" value="{{ $post->image }}">
+          @if($post->image)
+            <img src="{{ asset('storage/' . $post->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block">
+          @else
+            <img class="img-preview img-fluid mb-3 col-sm-5">
+          @endif
+          <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+          @error('image')
+            <div class="invalid-feedback">
+              {{ $message }} 
+            </div>
+          @enderror
+        </div>
+
         <div class="mb-3">
           <label for="body" class="form-label">Body</label>
           @error('body')
@@ -64,6 +84,20 @@
     document.addEventListener('trix-file-accept', function(e) {
         e.preventDefault();
     })
+
+    function previewImage() {
+    const image = document.querySelector('#image');
+    const imgPreview = document.querySelector('.img-preview');
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+    oFReader.onload = function(oFREvent) {
+        imgPreview.src = oFREvent.target.result;
+    }
+  }
 </script>
 
 @endsection
