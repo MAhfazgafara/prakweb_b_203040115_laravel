15b53c9df0ad014c3120d662237f4358246e097d
MAhfazgafara <m.ahfaz.203040115@mail.unpas.ac.id>
2022-12-15 19:04:35 +0700

    Authorization

--- applications/coba-laravel/app/Providers/AppServiceProvider.php
+++ applications/coba-laravel/app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 use Illuminate\Pagination\Paginator;
-
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        Gate::define('admin', function(User $user) {
+            return $user->is_admin;
+        });
     }
 }
